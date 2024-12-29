b7bb160157e4501396021ff6de838b521930f7a7
Yohannes D. Kassaye <Kassaye85@gmail.com>
2024-12-29 13:46:12 +0100

    wip - python import issue

--- backend/src/api.py
+++ backend/src/api.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from scraper.src.scrapers.PremierLeagueScraper import PremierLeagueScraper
 
 
-
 app = FastAPI()
 prem_scraper = PremierLeagueScraper()
 
@@ -18,5 +17,8 @@ app.add_middleware(
 
 @app.get("/api/data")
 async def get_data():
-    return prem_scraper.scrape().to_json()
+    data = prem_scraper.scrape()
+    print(data)
+    # return scraped data in JSON format
+    return data.to_json(orient="records")
 
